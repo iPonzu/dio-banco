cc265d1241ed0acd7fc8b4800d42dda4f1837992
iPonzu <joao_ignaciocastro1603@hotmail.com>
2025-03-21 17:02:38 -0300

    single commit

---
+++ src/Conta.java
@@ -0,0 +1,45 @@
+public abstract class Conta implements IConta {
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+    
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta (Cliente cliente){
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    @Override
+        public void sacar(double valor) {
+            saldo -= valor;
+        }
+    @Override
+        public void depositar(double valor){
+            saldo += valor;
+        }
+    @Override
+        public void transferir(double valor, IConta contaDestino){
+            this.sacar(valor);
+            contaDestino.depositar(valor);
+        }
+
+    public int getAgencia(){
+        return agencia;
+    }
+    public int getNumero(){
+        return numero;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+    protected void imprimirInfosComuns(){
+        System.out.println(String.format("Titular: ", this.cliente.getNome()));
+        System.out.println(String.format("Agencia: ", this.agencia));
+        System.out.println(String.format("Numero: ", this.numero));
+        System.out.println(String.format("Saldo", this.saldo));
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Cliente lucas = new Cliente();
+        lucas.setNome("Lucas");
+
+        Conta cc = new ContaCorrente(lucas);
+        Conta poupanca = new ContaPoupanca(lucas);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
